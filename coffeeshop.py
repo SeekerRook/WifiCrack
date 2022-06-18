f00e2d97e9eaa64f62895d2cb390b9c433696e9c
Kali Live user <xrist.vardakis@gmail.com>
2022-06-18 18:50:54 +0000

    updated password generator to use custom wordlists and phones

--- coffeeshop.py
+++ coffeeshop.py
@@ -1,4 +1,4 @@
-#! usr/bin/python3
+#! /usr/bin/python3
 from sys import argv 
 import datetime
 import argparse
@@ -20,17 +20,25 @@ def create(f,l,name):
             print (w+d)
             print(w+'-'+d)
     for w in wordlist:
+        
+        l = ['1','2','3','4','5']
         for i in range(5):
-            for j in ['1','2','3','4','5']:
-                print(w+j*i)
 
+            for n,j in enumerate(l):
+                print(w+j*i)
+        for i in range(10):    
+            print(w+ "".join(l[:i]))
+            print(w+"-"+"".join(l[:i]))
 f = 2005
 l = datetime.datetime.now().year
 
 ap = argparse.ArgumentParser()
-ap.add_argument("name" ,help="name of wifi or other keyword")
+ap.add_argument("-n","--name" ,action = 'append',help="name of wifi or other keyword")
 ap.add_argument("-f","--first",type=int,help ="first date(defaults to 2005")
 ap.add_argument("-l","--last",type=int,help="last date(drfaults to current year)")
+ap.add_argument("-p","--phone",action='append',help="phone/anything  else")
+ap.add_argument("-o","--load",help="load extra wordlist")
+
 args = vars(ap.parse_args())
 
 
@@ -43,11 +51,32 @@ lt = args["last"]
 #except ValueError:
 #    if (ft != "" or lt != ""):
 #        exit(1)
-   
-name = args["name"]
+common = {
+        "123456",
+        "123456789",
+        "qwerty",
+        "password",
+        "12345",
+        "qwerty123",
+        "1q2w3e",
+        "12345678",
+        "111111",
+        "1234567890"
 
+        }
+if (args["load"] != None):
+    with open(args["load"],'r') as load:
+        lines = load.readlines()
+        for line in lines:
+            common.add(line)
+if (args["phone"] != None):
+    for p in args["phone"]:
+        common.add(p)
+[print(c) for c in common]
 
-create(f,l,name)
+if args["name"] != None:
+    for name in args["name"]:
+        create(f,l,name)
 
     
 
