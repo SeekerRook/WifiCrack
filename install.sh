31f49a7edd06b8cc65fd1600c9916b9ecc839917
Kali Live user <xrist.vardakis@gmail.com>
2022-06-18 19:48:23 +0000

    fixed file structure

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 mkdir bin
-cp listen.sh ./bin/wclisten
-cp crack.sh ./bin/wccrack
-cp coffeeshop.py ./bin/coffeeshop
-cp deauth.sh ./bin/wcdeauth
-cp uninstall.sh ./bin/uninstallwc
+cp src/listen.sh ./bin/wclisten
+cp src/crack.sh ./bin/wccrack
+cp src/coffeeshop.py ./bin/coffeeshop
+cp src/deauth.sh ./bin/wcdeauth
+cp src/uninstall.sh ./bin/uninstallwc
 
 chmod +x ./bin/*
 sudo mv ./bin/* /usr/bin
